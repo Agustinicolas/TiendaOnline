7f1196663cc6c2e277336005e1dd563d056e0151
Agustinicolas <agustinicolas@outlook.com>
2023-11-08 14:34:54 -0300

    modificados links a productos de manera dinamica

--- js/funciones.js
+++ js/funciones.js
@@ -2,7 +2,9 @@ export function mostrarProductos(data, contenedor) {
     data.forEach((elem) => {
         let producto = document.createElement("a");/* Creacion elemento a con clase main__caja y direccion a pagina seleccion.html */
         producto.setAttribute("class","main__caja");
-        producto.setAttribute("href","./seleccion.html");
+        const categoria = elem.categoria;
+        const id = elem.id;
+        producto.setAttribute("href","./seleccion.html?categoria="+categoria+"&id="+id);
         
         producto.innerHTML=`
         <img src="${elem.imgurl}" alt="">
