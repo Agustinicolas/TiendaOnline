f3ae24859b0f211d1fabe62535ad3b879609d062
Agustinicolas <agustinicolas@outlook.com>
2023-11-23 04:35:02 -0300

    Mejoras varias

--- server/app.js
+++ server/app.js
@@ -2,36 +2,23 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
-const { products } = require('./routes/products.js');
+const fs = require('fs');
+let products = JSON.parse(fs.readFileSync('./products.json')); //JSON -> JS Object
 
 app.use(express.json() );
 app.use(express.static(path.join(__dirname , '/public')));
 
+//GET - /api/products
 app.get('/api/products', (req,res)=>{
-    res.json(products);
+    res.status(200).json({
+        status:"success",
+        data: {
+            products: products
+        }
+    })
 });
 
-//DEVUELVE PRODUCTO POR ID
-app.get('/api/products/:prodID', (req,res)=>{
-    const { prodID } = req.params;
-    const productoIndividual = products.find(
-        (product)=>product.id === prodID);
-    if(!productoIndividual){
-        return res.status(404).send('Producto no existe');
-    }
-    res.json(productoIndividual);
-});
 
-//DEVUELVE TODOS LOS PRODUCTOS DE UNA CATEGORIA
-app.get('/api/products/categories/:category', (req,res)=>{
-    const {category} = req.params;
-    const newProducts = products.filter(
-        (product)=> product.categoria === category);
-    if(newProducts.length == 0){
-        return res.status(404).send('Categoria no encontrada')
-    }
-    res.json(newProducts);
-});
 
 app.listen(
     port,
